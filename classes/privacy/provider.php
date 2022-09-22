6aa5aea0035d7e01c0c5708628e7bb61399652b4
prowebat <1819129+prowebat@users.noreply.github.com>
2022-09-22 14:14:12 +0200

    https://github.com/prowebat/moodle-mod_teamviewerclassroom/issues/2

---
+++ classes/privacy/provider.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Privacy class for requesting user data.
+ *
+ * @copyright 2022 onwards, TeamViewer Germany GmbH (contact@teamviewer.com)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_teamviewerclassroom\privacy;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/mod/teamviewerclassroom/locallib.php');
+
+use \core_privacy\local\metadata\collection;
+use \core_privacy\local\request\contextlist;
+use \core_privacy\local\request\writer;
+use \core_privacy\local\request\approved_contextlist;
+use \core_privacy\local\request\transform;
+use \core_privacy\local\request\helper;
+use \core_privacy\local\request\userlist;
+use \core_privacy\local\request\approved_userlist;
+use \core_privacy\manager;
+
+/**
+ * Privacy class for requesting user data.
+ *
+ * @package   mod_teamviewerclassroom
+ * @copyright 2022 onwards, TeamViewer Germany GmbH (contact@teamviewer.com)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements \core_privacy\local\metadata\null_provider {
+	/**
+	 * Get the language string identifier with the component's language
+	 * file to explain why this plugin stores no data.
+	 *
+	 * @return  string
+	 */
+	public static function get_reason() : string {
+		return 'privacy:metadata';
+	}
+}
